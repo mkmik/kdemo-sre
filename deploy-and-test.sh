47d0a1f16e1a12a1b747410b5e0d23b30aa39ca1
Marko Mikulicic <mmikulicic@gmail.com>
2020-04-30 15:50:27 +0200

    Restructure

--- deploy-and-test.sh
+++ deploy-and-test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+NS=${1:-prod}
+
 # reduce kapp log verbosity by polling less aggressively
 DEPLOY_FLAGS="--wait-check-interval=5s"
 
-kapp deploy -c -y -a kdemo -f app.yaml $DEPLOY_FLAGS
+knot8 set <vendor/kdemo/app.yaml --from=${NS}/values.yaml | kapp deploy -c -y -a kdemo -n ${NS} -f - $DEPLOY_FLAGS
 
 trap 'kill $(jobs -p)' EXIT
 
 kubectl proxy &
 sleep 2
 
-curl http://localhost:8001/api/v1/namespaces/default/services/kdemo/proxy/
+curl http://localhost:8001/api/v1/namespaces/${NS}/services/kdemo/proxy/
